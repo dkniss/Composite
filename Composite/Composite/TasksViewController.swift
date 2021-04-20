4c6bf2041b1c0d4bc328c8b3ecde4d79feb3a5b0
Daniil <danielkniss@mail.ru>
2021-04-20 22:52:54 +0500

    added TasksViewController extension

--- Composite/Composite/TasksViewController.swift
+++ Composite/Composite/TasksViewController.swift
@@ -80,17 +80,3 @@ class TasksViewController: UITableViewController,UINavigationControllerDelegate 
     }
 }
 
-protocol TasksDelegate: AnyObject {
-    func updateTasks(with tasks : [CompositeTask])
-}
-
-extension TasksViewController: TasksDelegate {
-    func updateTasks(with tasks: [CompositeTask]) {
-        if let previousIndexPath = self.previousTaskIndexPath {
-            self.tasks[previousIndexPath.row].tasks = tasks
-            self.tableView.reloadRows(at: [previousIndexPath], with: .automatic)
-        }
-    }
-    
-    
-}
